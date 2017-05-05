7c02225c125a8c0d0521c6f91cdcdaf194601227
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-05-05 03:11:20 -0400

    modifies files

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// Random number is generated at start of game. This number can be any integer between 19 and 120
+// Crystals are images, and also clickable buttons (bootstrap required). A click of any crystal will result in adding a predetermined random amount to your score. This random amount stays fixed until the game ends, after which it is reset to a different value 
+// Crystals are randomly generated numbers between 1 and 12.
+// Win game once score === random number. Lose game if score > random number.
+// Updates the stats for wins, losses. Reset number to a different random number.
