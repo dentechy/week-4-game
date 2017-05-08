1db489a1df578e49fd0c2fc9776bddd0cca0a8eb
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-05-08 00:25:58 -0400

    continues setting up javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,13 @@
 // Updates the stats for wins, losses. Reset number to a different random number.
 
 var randomTarget = 0;
+var rubyGem = 0;
+var emeraldGem = 0;
+var sapphireGem = 0;
+var topazGem = 0;
+var userScore = 0;
+var wins = 0;
+var losses = 0;
 
 
 //Functions to generate random numbers and keep track of score.
@@ -13,11 +20,51 @@ function getRandomNumber() {
     randomTarget = Math.floor(Math.random() * (120 - 19)) + 19;
     $("#randomTarget").html(randomTarget);
 }
-console.log(randomTarget);
 
+function gemButtons() {
+    rubyGem = Math.floor(Math.random() * (13 - 1)) + 1;
+    emeraldGem = Math.floor(Math.random() * (13 - 1)) + 1;
+    sapphireGem = Math.floor(Math.random() * (13 - 1)) + 1;
+    topazGem = Math.floor(Math.random() * (13 - 1)) + 1;
+} 
 
-
-
+//This function below ensures that none of the buttons have the same number.
+function checkEquality() {
+    var gemArr = [rubyGem, emeraldGem, sapphireGem, topazGem];
+//Need to iterate through the array, checking a pair of variables at a time for equality. A total of 6 comparisons need to be made to account for all possible combinations. 
+    for (i = 0; i < gemArr.length; i++) {
+        if (gemArr[i] === gemArr[i+1]) {
+            gemButtons(); //runs the randomization function again
+        }
+        else if (gemArr[i] === gemArr[i+2]) {
+            gemButtons();
+        }
+        else if (gemArr[i] === gemArr[i+3]) {
+            gemButtons();
+        }
+    }
+}
 // The Main Process
 
 getRandomNumber();
+
+gemButtons();
+checkEquality();
+
+$("#ruby").click(function() {
+    userScore = userScore + rubyGem;
+    $("#userScore").html("<p>" + userScore + "</p>");
+});
+
+$("#emerald").click(function() {
+    userScore = userScore + emeraldGem;
+    $("#userScore").html("<p>" + userScore + "</p>");
+});
+$("#sapphire").click(function() {
+    userScore = userScore + sapphireGem;
+    $("#userScore").html("<p>" + userScore + "</p>");
+});
+$("#topaz").click(function() {
+    userScore = userScore + topazGem;
+    $("#userScore").html("<p>" + userScore + "</p>");
+});
